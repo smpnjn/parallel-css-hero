59b83cd2b2f7551d318a7d2b01617eaf8fe764fa
Johnny Simpson <johnnysimpson@Johnnys-MacBook-Pro.local>
2020-12-21 22:14:11 +0000

    First Commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # webgl-gradient
 A cool gradient effect created in WebGL and Javascript
 
-Medium article can be found here: https://medium.com/@smpnjn/apple-music-like-cards-with-webgl-6256de5aa32
+Fjolt article can be found here: https://fjolt.com/article/css-parallax-3d-image-composition
--- script.js
+++ script.js
@@ -43,54 +43,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
     ['pointerup', 'pointerleave'].forEach(function(item) {
         
-        document.body.addEventListener(item, function(e) {/*
-            let startXNeg, startXPos, startYNeg, startYPos;
-            reduceRotate = setInterval(function() {
-                let finishX, finishY;
-                let change = 1;
-                if(typeof document.getElementById('rotator').style.transform.split('rotateY(')[1] !== "undefined") {
-                    curValueY = parseFloat(document.getElementById('rotator').style.transform.split('rotateY(')[1].split(')')[0]);
-                }
-                if(typeof document.getElementById('rotator').style.transform.split('rotateX(')[1] !== "undefined") {
-                    curValueX = parseFloat(document.getElementById('rotator').style.transform.split('rotateX(')[1].split(')')[0]);
-                }
-    
-                console.log(curValueX );
-                
-                if(curValueX > 0) {
-                    if(startXPos !== true) {
-                        curValueX -= change;
-                    } else { finishX = true; }
-                    startXNeg = true;
-                }
-                else if(curValueX < 0) {
-                    if(startXNeg !== true) {
-                        curValueX += change;
-                    } else { finishX = true; }
-                    startXPos = true;
-                }
-
-                if(curValueY > 0) {
-                    if(startYNeg !== true) {
-                        curValueY -= change;
-                    } else { finishY = true; }
-                    startYPos = true;
-                }
-                else if(curValueY < 0) {
-                    if(startYPos !== true) {
-                        curValueY += change;
-                    } else { finishY = true; }
-                    startYNeg = true;
-                }
-
-                document.getElementById('rotator').style.transform = `rotateY(${curValueY}deg) rotateX(${curValueX}deg)`;
-
-                if(finishX == true && finishY == true || curValueX == 0 && curValueY == 0) {
-                    clearInterval(reduceRotate);
-                    document.getElementById('rotator').style.transform = `rotateY(0deg) rotateX(0deg)`;
-                }
-            }, 1000/60);
-            console.log(typeof reduceRotate);*/
+        document.body.addEventListener(item, function(e) {
             drag = false;
             document.getElementById('rotator').classList.remove('rotating');
         });
